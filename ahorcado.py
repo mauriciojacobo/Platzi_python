c4ad6bb0e12cf5e3c9e8893853ee738b2be1ae30
Mauricio Jácobo <jacobomauricio044@gmail.com>
2021-05-06 15:37:02 -0500

    se agrego la funcion principal y se creo la ruta a data.txt

--- ahorcado.py
+++ ahorcado.py
@@ -1,12 +1,26 @@
 import os
 import random
 
-def read():
+def read(filepath = "./archivos/data.txt"):
     words = []
-    with open("./data.txt", "r", encoding= "utf-8") as f:
-        [words for word in f]
+    with open(filepath, "r", encoding= "utf-8") as f:
+        [words.append(word.replace("\n","")) for word in f]       
     return words
-    
+
+
+def run ():
+    data = read(filepath="./archivos/data.txt")
+    random_word = random.choice(data)
+    random_word_list = [letter for letter in random_word]
+    underscores = "_" * len(random_word)
+    random_word_dict = {}
+    for idx, letter in enumerate(random_word):
+        if not random_word_dict.get(letter):
+            random_word_dict[letter] = []
+        random_word_dict(letter).append(idx)
+
+palabras = read()
+print(palabras)
 
 
 

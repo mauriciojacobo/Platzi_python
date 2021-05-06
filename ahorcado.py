6962c113e2930e4f811730601976b98dce14a747
Mauricio Jácobo <jacobomauricio044@gmail.com>
2021-05-06 16:14:47 -0500

    con la funcion normalize

--- ahorcado.py
+++ ahorcado.py
@@ -1,5 +1,6 @@
 import os
 import random
+import time
 
 def read(filepath = "./archivos/data.txt"):
     words = []
@@ -7,8 +8,19 @@ def read(filepath = "./archivos/data.txt"):
         [words.append(word.replace("\n","")) for word in f]       
     return words
 
+def normalize(s):
+    replacement = (
+        ("a","á"),
+        ("e","é"),
+        ("i","í"),
+        ("o","ó"),
+        ("u","ú"),
+    )
+    for a, b in replacement:
+        s = s.replace(a, b)
+    return s
 
-def run ():
+def run():
     data = read(filepath="./archivos/data.txt")
     random_word = random.choice(data)
     random_word_list = [letter for letter in random_word]
@@ -17,10 +29,32 @@ def run ():
     for idx, letter in enumerate(random_word):
         if not random_word_dict.get(letter):
             random_word_dict[letter] = []
-        random_word_dict(letter).append(idx)
+        random_word_dict[letter].append(idx)
 
-palabras = read()
-print(palabras)
+    while True:
+        os.system("cls")
+        print("¡Adivina la palabra!")
+        for element in underscores:
+            print(element + " ", end="")
+        print("\n")
 
+        letter =input("Ingresa una letra: ").strip().upper()
+        assert letter.isalpha(), "Solo puedes insertar letras"
+        assert len(letter) == 1, "Ingresa solo una letra"
+
+        if letter in random_word_list:
+            for idx in random_word_dict[letter]:
+                underscores[idx] = letter
+                
+        # if letter not in random_word_list:
+        #     print("La palabra no contiene " + letter + ". Prueba otra letra")
+        #     time.sleep(2)
+        
+        if "_" not in underscores:
+            os.system("cls")
+            print("Ganaste el juego " + "\n" + "La Palabra era " + random_word)
+
+if __name__ == '__main__':
+    run()
 
 
